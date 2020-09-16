8a4316934d9363dd9e4dbdc731237c0cf7edac35
Nidhish1407 <nidhishp07@gmail.com>
2020-09-16 22:07:54 +0530

    Added darkerEveryTime button

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ const box = document.querySelector(".box");
 let gridSize=18;
 const canvasSize=600;
 let color = 1;
+let toggle = 1;
 
 //Reset grid
 const reset = document.querySelector(".reset");
@@ -20,7 +21,8 @@ function createGrid(gridSize=18)
            square.classList.add("square");
            let squareSize = canvasSize/gridSize;
            square.setAttribute('style',`width:${squareSize};height:${squareSize};border:1px solid grey;margin:0`);
-           square.addEventListener('mouseenter',colorGrid);
+           square.addEventListener('mouseenter',colorGrid); 
+
            column.appendChild(square);
        }
        box.appendChild(column);
@@ -41,14 +43,18 @@ mono.addEventListener('click',()=> color = 1);
 
 const colorful = document.querySelector(".color");
 colorful.addEventListener('click',()=>color = 2);
+
+const darker = document.querySelector(".dark");
+darker.addEventListener('click',()=>color = 3);
 function colorGrid(e)
 {   
     switch(color)
     {
-        case 1 : e.target.style.backgroundColor = 'red';
+        case 1 : e.target.style.backgroundColor = 'rgb(212, 70, 17)';//red
                  break;
         case 2 : e.target.style.backgroundColor = randomColor();
                  break;
+        case 3 : e.target.style.backgroundColor = darkerEverytime(e.target.style.backgroundColor);
     }
 };
 function randomColor()
@@ -58,7 +64,22 @@ function randomColor()
     let green = Math.ceil(Math.random()*255);
     return `rgb(${red},${blue},${green})`;
 }
+function darkerEverytime(colour)
+{ 
+   if(colour == "")
+   color;
+   else
+   {
+       let temp = colour.slice(4,colour.length-1);
+       temp = temp.split(',');
+       let red = +temp[0] - 25;
+       let blue = +temp[1] - 25;
+       let green = +temp[2] - 25;
+       return `rgb(${red},${blue},${green})`;
+   }
+}
 
+//Resets Grid
 function clearGrid(e)
 {
     box.innerHTML="";
